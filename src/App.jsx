346a5f1c8eedea69938987523f8ad6ec5b4588d1
Aaditya-2407 <kharataaditya2407@gmail.com>
2026-03-03 23:17:46 +0530

    feat:chatbot

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,27 @@
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import BotpressChat from './components/BotpressChat';
 
 function App() {
-  
   const emptyRefs = { heroRef: null, eventRef: null, aboutRef: null, scheduleRef: null };
   const dummyScroll = () => {};
 
   return (
     <div className="bg-black min-h-screen flex flex-col justify-between">
-       {/* Navbar */}
       <Navbar 
         scrollToRefs={emptyRefs} 
         scrollToSection={dummyScroll} 
         isScrolled={true} 
       />
       
-      
       <main className="flex-grow flex items-center justify-center">
-         <h1 className="text-white text-5xl font-bold tracking-widest">
-            ACUNETIX 13.0
-         </h1>
+        <h1 className="text-white text-5xl font-bold tracking-widest">
+          ACUNETIX 13.0
+        </h1>
       </main>
 
-      {/* Footer */}
       <Footer scrollToRefs={emptyRefs} scrollToSection={dummyScroll} />
+      <BotpressChat />
     </div>
   );
 }
